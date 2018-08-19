e8f4228a34913eeb72b642f88689768fbcfbb7e3
AngelCarvajal <AngelCarvajal@users.noreply.github.com>
2018-08-19 00:08:57 -0400

    css

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	var navbarCollapse = function() {
+    	if ($("#mainNav").offset().top > 100) {
+    		$('nav.navbar div a img#logoBlanco').css('display', 'none');
+    		$('nav.navbar div a img#logoNegro').css('display', 'block');
+      		$("#mainNav").addClass("navbar-shrink");
+    	} else {
+    		$('nav.navbar div a img#logoBlanco').css('display', 'block');
+    		$('nav.navbar div a img#logoNegro').css('display', 'none');
+      		$("#mainNav").removeClass("navbar-shrink");
+    	}
+  	};
+  	// Collapse now if page is not at top
+  	navbarCollapse();
+  	// Collapse the navbar when page is scrolled
+  	$(window).scroll(navbarCollapse);
+});
--- js/grayscale.js
+++ js/grayscale.js
@@ -27,18 +27,16 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-      console.log("fdrtg");
-      $("#logowhite").prepend("<img src= 'img/black-logo.png' width='30' height='30' class='d-inline-block align-top'>");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  // var navbarCollapse = function() {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // };
+  // // Collapse now if page is not at top
+  // navbarCollapse();
+  // // Collapse the navbar when page is scrolled
+  // $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
