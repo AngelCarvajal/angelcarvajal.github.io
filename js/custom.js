40d960f74094d3c523558207eab2efe77c96972e
AngelCarvajal <AngelCarvajal@users.noreply.github.com>
2018-08-20 15:59:15 -0400

    Look preview

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
-  //Titulo de las redes sociales
-  $('[data-toggle="tooltip"]').tooltip();
+    //Funcion que cambia el logo en la barra de navegacion
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $('nav.navbar div a img#logoBlanco').css('display', 'none');
+            $('nav.navbar div a img#logoNegro').css('display', 'block');
+            $("#mainNav").addClass("navbar-shrink");
+            console.log('La tiene');
+        } else {
+            $('img#logoNegro').css('display', 'none');
+            $('img#logoBlanco').css('display', 'block');
+            $("#mainNav").removeClass("navbar-shrink");
+            console.log('No la tiene');
+        }
+    };
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
 
-  //funcion para la barra de navegacion y el logo
-	var navbarCollapse = function() {
-    	if ($("#mainNav").offset().top > 100) {
-    		$('nav.navbar div a img#logoBlanco').css('display', 'none');
-    		$('nav.navbar div a img#logoNegro').css('display', 'block');
-      		$("#mainNav").addClass("navbar-shrink");
-    	} else {
-    		$('nav.navbar div a img#logoBlanco').css('display', 'block');
-    		$('nav.navbar div a img#logoNegro').css('display', 'none');
-      		$("#mainNav").removeClass("navbar-shrink");
-    	}
-  	};
-  	// Collapse now if page is not at top
-  	navbarCollapse();
-  	// Collapse the navbar when page is scrolled
-  	$(window).scroll(navbarCollapse);
 });
